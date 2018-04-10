8d808315d3cb30356814348a896477ec0453b35e
MuruganSiva <haishiva@gmail.com>
2018-04-10 16:03:02 -0400

    ColorCode.App

---
+++ ColorCode.App/Controllers/OhmValueController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ColorCode.Provider.Contracts;
+using ColorCode.Provider.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ColorCode.App.Controllers
+{
+    /// <summary>
+    /// Ohm Value Controller
+    /// </summary>
+    [Route("ohmvalue")]
+    public class OhmValueController : Controller
+    {
+        #region Private Members
+        private readonly IOhmProvider _provider;
+        #endregion
+
+        public OhmValueController(IOhmProvider provider)
+        {
+            _provider = provider ?? throw new Exception("OhmProvider is NULL");
+        }
+
+        #region Action Methods
+        [Route("index")]
+        public IActionResult Index(PageModel model)
+        {
+            PageModel viewModel = _provider.InitializeModel();
+            return View(viewModel);
+        }
+
+        [Route("calculate")]
+        public IActionResult Calculate(PageModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseModel responseModel = _provider.GetDataAsync(model);
+                model = _provider.InitializeModel();
+                model.OhmValue = responseModel.OhmValue.ToString();
+            }
+            return View("index", model);
+        }
+        #endregion
+    }
+}
